ba1aff62f7f928a566c208640b8810ff3ba7eb2f
Olga <govorkova.t@yandex.ru>
2023-10-28 19:19:15 +0300

    fix

---
+++ FitnessClub/FitnessClub.DataAccess/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using FitnessClub.DataAccess.Entities;
+
+namespace FitnessClub.DataAccess;
+
+public interface IRepository<T> where T: BaseEntity
+{
+    IQueryable<T> GetAll();
+    T? GetById(int id);
+    T? GetById(Guid id);
+    T Save(T entity);
+    void Delete(T entity);
+}
